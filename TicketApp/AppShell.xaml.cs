345502c92f34c3fbd3ac34dabddf16f22824f70d
jakob.roessler@stud.uni-regensburg.de <jakobs3mini@gmail.com>
2025-12-01 17:01:55 +0100

    initial commit

---
+++ TicketApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TicketApp;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ TicketApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace TicketApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
