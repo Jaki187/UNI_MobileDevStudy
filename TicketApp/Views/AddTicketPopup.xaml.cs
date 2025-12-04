3a0645ace136798b4018de799d3a6904a644000c
jakob.roessler@stud.uni-regensburg.de <jakobs3mini@gmail.com>
2025-12-04 12:28:05 +0100

    guest list available

---
+++ TicketApp/Views/AddTicketPopup.xaml.cs
@@ -0,0 +1,11 @@
+using CommunityToolkit.Maui.Views;
+
+namespace TicketApp.Views;
+
+public partial class AddTicketPopup : Popup
+{
+    public AddTicketPopup()
+    {
+        InitializeComponent();
+    }
+}
